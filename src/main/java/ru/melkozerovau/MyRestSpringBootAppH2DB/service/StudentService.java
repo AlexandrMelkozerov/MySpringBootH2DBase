1f952c88d6ed32e67a612ccec1915d54f48fd60c
Melkozerov <gamesartwork@mail.ru>
2024-01-08 14:15:19 +0500

    lab6

---
+++ src/main/java/ru/melkozerovau/MyRestSpringBootAppH2DB/service/StudentService.java
@@ -0,0 +1,14 @@
+package ru.melkozerovau.MyRestSpringBootAppH2DB.service;
+
+import org.springframework.stereotype.Service;
+import ru.melkozerovau.MyRestSpringBootAppH2DB.entity.Student;
+
+import java.util.List;
+
+@Service
+public interface StudentService {
+    List<Student> getAllStudents();
+    Student getStudent(int id);
+    Student saveStudent(Student student);
+    void deleteStudent(int id);
+}
